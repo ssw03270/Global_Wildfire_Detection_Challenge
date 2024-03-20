39ccca87aa6a67b83e30c2fb2be36cd9a5a57a29
SeungWon Seo <ttd8591@gmail.com>
2024-03-20 13:10:14 +0900

    Init

---
+++ source/dataloader.py
@@ -0,0 +1,77 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import os
+import rasterio
+
+MAX_PIXEL_VALUE = 65535 # 이미지 정규화를 위한 픽셀 최대값
+
+class ImageDatasets(Dataset):
+    def __init__(self, data_type='train'):
+        """
+        Initializes an instance of the ImageDatasets class.
+
+        Parameters:
+        - data_type (str): Specifies the type of the data ('train', 'test', 'val'), which determines the folder from which data is loaded.
+        """
+
+        super(ImageDatasets, self).__init__()
+
+        self.data_type = data_type
+
+        self.input_folder_path = f'../dataset/train_img/'
+        self.output_folder_path = f'../dataset/train_mask/'
+
+        self.input_files = [f for f in os.listdir(self.input_folder_path) if f.endswith('.tif')]
+        self.output_files = [f for f in os.listdir(self.output_folder_path) if f.endswith('.tif')]
+
+        self.input_files.sort()
+        self.output_files.sort()
+
+        self.data_length = len(self.input_files)
+        self.val_length = 0.8 * self.data_length
+
+        if data_type == 'train':
+            self.input_files = self.input_files[:int(self.val_length)]
+            self.output_files = self.output_files[:int(self.val_length)]
+        elif data_type == 'val':
+            self.input_files = self.input_files[int(self.val_length):]
+            self.output_files = self.output_files[int(self.val_length):]
+
+        self.data_length = len(self.input_files)
+
+        print(self.data_length)
+
+    def __getitem__(self, idx):
+        """
+        Retrieves a single item from the dataset at the specified index.
+
+        Parameters:
+        - idx (int): The index of the item to retrieve.
+
+        Returns:
+        - dict: A dictionary containing the padded matrices and padding masks for boundary adjacency matrix, building adjacency matrix, and boundary-building adjacency matrix, as well as the boundary positions and the number of boundaries and buildings. For test data, it also returns the filename of the loaded pickle file.
+        """
+
+        load_path = self.input_folder_path + '/' + self.input_files[idx]
+        with open(load_path, 'rb') as f:
+            img = rasterio.open(load_path).read().transpose((1, 2, 0))
+            input_img = np.float32(img)/MAX_PIXEL_VALUE
+
+        load_path = self.output_folder_path + '/' + self.output_files[idx]
+        with open(load_path, 'rb') as f:
+            img = rasterio.open(load_path).read().transpose((1, 2, 0))
+            output_img = np.float32(img) / MAX_PIXEL_VALUE
+
+        return {'input_img': input_img, 'output_img': output_img}
+
+
+    def __len__(self):
+        """
+        Returns the total number of items in the dataset.
+
+        Returns:
+        - int: The total number of items in the dataset.
+        """
+
+        return self.data_length
---
+++ source/train.py
@@ -0,0 +1,347 @@
+import os
+from datetime import datetime
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.tensorboard import SummaryWriter
+import torch.distributed as dist
+
+import numpy as np
+import random
+
+from model import SwinTransformer
+from dataloader import ImageDatasets
+
+import wandb
+from tqdm import tqdm
+
+class Trainer:
+    def __init__(self, batch_size, max_epoch, use_checkpoint, checkpoint_epoch, use_tensorboard, dropout,
+                 val_epoch, save_epoch, local_rank, save_dir_path, lr, d_model, n_layer, n_head, weight_decay):
+        """
+        Initializes the Trainer class.
+
+        Parameters:
+        - batch_size (int): Batch size for training and validation.
+        - max_epoch (int): Maximum number of epochs for training.
+        - use_checkpoint (bool): Whether to use a pre-trained model checkpoint.
+        - checkpoint_epoch (int): The epoch of the checkpoint to use.
+        - use_tensorboard (bool): Whether to log data to TensorBoard.
+        - dropout (float): Dropout rate used in the transformer model.
+        - val_epoch (int): Frequency of validation in terms of epochs.
+        - save_epoch (int): Frequency of saving the model in terms of epochs.
+        - local_rank (int): The rank of the current process in the distributed training setup.
+        - save_dir_path (str): Path to save the model checkpoints.
+        - lr (float): Learning rate for the optimizer.
+        - d_model (int): Dimension of the model (embeddings).
+        - n_layer (int): Number of layers in the transformer model.
+        - n_head (int): Number of attention heads in the transformer model.
+        - weight_decay (float): Weight decay (L2 penalty) for the optimizer.
+
+        Initializes training and validation datasets, dataloaders, model, and optimizer. Sets up distributed training if applicable.
+        """
+
+        self.batch_size = batch_size
+        self.max_epoch = max_epoch
+        self.use_checkpoint = use_checkpoint
+        self.checkpoint_epoch = checkpoint_epoch
+        self.use_tensorboard = use_tensorboard
+        self.val_epoch = val_epoch
+        self.save_epoch = save_epoch
+        self.local_rank = local_rank
+        self.save_dir_path = save_dir_path
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.n_head = n_head
+        self.d_model = d_model
+        self.n_layer = n_layer
+        self.dropout = dropout
+
+        print('local_rank', self.local_rank)
+
+        self.device = torch.device(f'cuda:{self.local_rank}') if torch.cuda.is_available() else torch.device('cpu')
+
+        self.train_dataset = ImageDatasets(data_type='train')
+        self.train_sampler = DistributedSampler(dataset=self.train_dataset, rank=rank, shuffle=True)
+        self.train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size,
+                                           sampler=self.train_sampler, num_workers=8, pin_memory=True)
+
+        self.val_dataset = ImageDatasets(data_type='val')
+        self.val_sampler = DistributedSampler(dataset=self.val_dataset, rank=rank, shuffle=False)
+        self.val_dataloader = DataLoader(self.val_dataset, batch_size=self.batch_size,
+                                         sampler=self.val_sampler, num_workers=8, pin_memory=True)
+
+        self.transformer = SwinTransformer(
+            hidden_dim=96,
+            layers=(2, 2, 6, 2),
+            heads=(3, 6, 12, 24),
+            channels=10,
+            img_size=256,
+            head_dim=32,
+            window_size=8,
+            downscaling_factors=(4, 2, 2, 2),
+            relative_pos_embedding=True
+        ).to(device=self.device)
+        self.transformer = nn.parallel.DistributedDataParallel(self.transformer, device_ids=[local_rank])
+
+        self.optimizer = torch.optim.Adam(self.transformer.module.parameters(),
+                                          lr=self.lr,
+                                          betas=(0.9, 0.98))
+
+    def cross_entropy_loss(self, pred, trg):
+        """
+        Calculates the cross-entropy loss with padding mask applied.
+
+        Parameters:
+        - pred (Tensor): Predictions from the model.
+        - trg (Tensor): Target values.
+        - pad_mask (Tensor): Padding mask to ignore certain positions in the input.
+
+        Returns:
+        - Tensor: The average loss computed over all non-masked elements.
+        """
+        loss = F.binary_cross_entropy(pred, trg, reduction='mean')
+
+        return loss
+
+    def correct_data(self, pred, trg):
+        """
+        Computes the number of correctly predicted data points, considering the padding.
+
+        Parameters:
+        - pred (Tensor): Predictions from the model, with a threshold applied to determine binary outcomes.
+        - trg (Tensor): Target values.
+        - pad_mask (Tensor): Padding mask to consider only non-padded elements in the accuracy calculation.
+
+        Returns:
+        - tuple: A tuple containing the number of correct predictions and the number of non-masked elements, used to compute accuracy.
+        """
+
+        predictions = (pred >= 0.5).float()
+
+        correct_predictions = (predictions == trg).float()
+        issue_predictions = (predictions != trg).float()
+
+        return correct_predictions.sum().item(), correct_predictions.sum().item() + issue_predictions.sum().item()
+
+    def train(self):
+        """
+        Executes the training loop over the dataset for a specified number of epochs.
+        Validates the model at specified intervals, saves model checkpoints, and logs to TensorBoard/WandB.
+
+        Returns:
+        None
+        """
+
+        epoch_start = 0
+        min_loss = 99999999999
+        early_stop_count = 0
+
+        if self.use_checkpoint:
+            checkpoint = torch.load("./models/transformer/epoch_" + str(self.checkpoint_epoch) + ".pth")
+            self.transformer.module.load_state_dict(checkpoint['model_state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            epoch_start = checkpoint['epoch']
+
+        if self.use_tensorboard:
+            self.writer = SummaryWriter()
+            # if self.local_rank == 0:
+            #     wandb.watch(self.transformer.module, log='all')
+
+        for epoch in tqdm(range(epoch_start, self.max_epoch)):
+            total_loss = torch.Tensor([0.0]).to(self.device)
+            total_correct = torch.Tensor([0.0]).to(self.device)
+            total_problem = torch.Tensor([0.0]).to(self.device)
+
+            for data in self.train_dataloader:
+                self.optimizer.zero_grad()
+
+                input_img = data['input_img'].to(device=self.device)
+                output_img = data['output_img'].to(device=self.device)
+
+                output = self.transformer(input_img)
+
+                exist_loss = self.cross_entropy_loss(output, output_img.detach())
+                loss = exist_loss
+                correct, problem = self.correct_data(output.detach(), output_img.detach())
+
+                loss.backward()
+                self.optimizer.step()
+
+                dist.all_reduce(loss, op=dist.ReduceOp.SUM)
+                dist.all_reduce(torch.tensor(correct), op=dist.ReduceOp.SUM)
+                dist.all_reduce(torch.tensor(problem), op=dist.ReduceOp.SUM)
+
+                total_loss += loss
+                total_correct += correct
+                total_problem += problem
+                # if self.local_rank == 0:
+                #     print(loss_pos, loss_size, loss_kl)
+
+            if self.local_rank == 0:
+                loss_mean = total_loss.item() / (len(self.train_dataloader) * dist.get_world_size())
+                correct_mean = total_correct.item() / total_problem.item()
+
+                print(f"Epoch {epoch + 1}/{self.max_epoch} - Loss Total : {loss_mean:.4f}")
+                print(f"Epoch {epoch + 1}/{self.max_epoch} - Accuracy : {correct_mean:.4f}")
+
+                if self.use_tensorboard:
+                    wandb.log({"Train loss total": loss_mean}, step=epoch + 1)
+                    wandb.log({"Train accuracy": correct_mean}, step=epoch + 1)
+
+            if (epoch + 1) % self.val_epoch == 0:
+                self.transformer.module.eval()
+                total_loss = torch.Tensor([0.0]).to(self.device)
+                total_correct = torch.Tensor([0.0]).to(self.device)
+                total_problem = torch.Tensor([0.0]).to(self.device)
+
+                with torch.no_grad():
+                    for data in self.val_dataloader:
+
+                        input_img = data['input_img'].to(device=self.device)
+                        output_img = data['output_img'].to(device=self.device)
+
+                        output = self.transformer(input_img)
+
+                        exist_loss = self.cross_entropy_loss(output.detach(), output_img.detach())
+                        loss = exist_loss
+                        correct, problem = self.correct_data(output.detach(), output_img.detach())
+
+                        dist.all_reduce(loss, op=dist.ReduceOp.SUM)
+                        dist.all_reduce(torch.tensor(correct), op=dist.ReduceOp.SUM)
+                        dist.all_reduce(torch.tensor(problem), op=dist.ReduceOp.SUM)
+
+                        total_loss += loss
+                        total_correct += correct
+                        total_problem += problem
+
+                    if self.local_rank == 0:
+                        loss_mean = total_loss.item() / (len(self.val_dataloader) * dist.get_world_size())
+                        correct_mean = total_correct.item() / total_problem.item()
+
+                        print(f"Epoch {epoch + 1}/{self.max_epoch} - Validation Loss Total : {loss_mean:.4f}")
+                        print(f"Epoch {epoch + 1}/{self.max_epoch} - Validation Accuracy : {correct_mean:.4f}")
+
+                        if self.use_tensorboard:
+                            wandb.log({"Validation loss total": loss_mean}, step=epoch + 1)
+                            wandb.log({"Validation accuracy": correct_mean}, step=epoch + 1)
+
+                            loss_total = loss_mean
+                            if min_loss > loss_total:
+                                min_loss = loss_total
+                                checkpoint = {
+                                    'epoch': epoch,
+                                    'model_state_dict': self.transformer.module.state_dict(),
+                                    'optimizer_state_dict': self.optimizer.state_dict(),
+                                }
+
+                                save_path = os.path.join("./models", self.save_dir_path)
+                                if not os.path.exists(save_path):
+                                    os.makedirs(save_path)
+                                torch.save(checkpoint, os.path.join(save_path, "epoch_best.pth"))
+
+                                early_stop_count = 0
+                            else:
+                                early_stop_count += 1
+                                if early_stop_count >= self.max_epoch / 10:
+                                    break
+
+                self.transformer.module.train()
+
+            if (epoch + 1) % self.save_epoch == 0:
+                checkpoint = {
+                    'epoch': epoch,
+                    'model_state_dict': self.transformer.module.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict(),
+                }
+
+                if self.local_rank == 0:
+                    save_path = os.path.join("./models", self.save_dir_path)
+                    if not os.path.exists(save_path):
+                        os.makedirs(save_path)
+                    torch.save(checkpoint, os.path.join(save_path, "epoch_" + str(epoch + 1) + ".pth"))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Initialize a transformer with user-defined hyperparameters.")
+
+    parser.add_argument("--batch_size", type=int, default=1, help="The batch size used during training.")
+    parser.add_argument("--max_epoch", type=int, default=1000, help="The maximum number of epochs to train for.")
+    parser.add_argument("--d_model", type=int, default=512, help="The dimensionality of the model's embeddings.")
+    parser.add_argument("--n_layer", type=int, default=6, help="The number of layers in the transformer model.")
+    parser.add_argument("--n_head", type=int, default=8, help="The number of attention heads in the transformer model.")
+    parser.add_argument("--dropout", type=float, default=0.1, help="The dropout rate used in the transformer model.")
+    parser.add_argument("--seed", type=int, default=327,
+                        help="A seed for random number generation to ensure reproducibility.")
+    parser.add_argument("--use_tensorboard", type=bool, default=True, help="Flag to enable logging to TensorBoard..")
+    parser.add_argument("--use_checkpoint", type=bool, default=False,
+                        help="Flag to enable loading the model from a checkpoint.")
+    parser.add_argument("--checkpoint_epoch", type=int, default=0,
+                        help="The epoch number of the checkpoint to load for training continuation.")
+    parser.add_argument("--val_epoch", type=int, default=1,
+                        help="Frequency (in epochs) with which to perform validation.")
+    parser.add_argument("--save_epoch", type=int, default=10,
+                        help="Frequency (in epochs) with which to save model checkpoints.")
+    parser.add_argument("--local-rank", type=int,
+                        help="The local rank of the process for distributed training. Required for multi-GPU setups.")
+    parser.add_argument("--save_dir_path", type=str, default="transformer_graph",
+                        help="Directory path where model checkpoints will be saved")
+    parser.add_argument("--lr", type=float, default=3e-5, help="The learning rate for the optimizer")
+    parser.add_argument("--weight_decay", type=float, default=5e-4,
+                        help="The weight decay (L2 penalty) used by the optimizer")
+
+    opt = parser.parse_args()
+
+    if opt.local_rank == 0:
+        wandb.login(key='5a8475b9b95df52a68ae430b3491fe9f67c327cd')
+        wandb.init(project='wildfire', config=vars(opt))
+
+        for key, value in wandb.config.items():
+            setattr(opt, key, value)
+
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    opt.save_dir_path = f"{opt.save_dir_path}_{current_time}"
+
+    if opt.local_rank == 0:
+        save_path = os.path.join("./models", opt.save_dir_path)
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        config_file_path = os.path.join(save_path, "config.txt")
+        with open(config_file_path, "w") as f:
+            for arg in vars(opt):
+                f.write(f"{arg}: {getattr(opt, arg)}\n")
+
+    if opt.local_rank == 0:
+        for arg in vars(opt):
+            print(f"{arg}: {getattr(opt, arg)}")
+
+    random.seed(opt.seed)
+    np.random.seed(opt.seed)
+    torch.manual_seed(opt.seed)
+    torch.cuda.manual_seed_all(opt.seed)
+
+    rank = opt.local_rank
+    torch.cuda.set_device(rank)
+    if not dist.is_initialized():
+        if torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu') == "cuda:0":
+            dist.init_process_group("gloo")
+
+        else:
+            dist.init_process_group('nccl')
+
+    trainer = Trainer(batch_size=opt.batch_size, max_epoch=opt.max_epoch,
+                      d_model=opt.d_model, n_layer=opt.n_layer, n_head=opt.n_head, dropout=opt.dropout,
+                      use_tensorboard=opt.use_tensorboard,
+                      use_checkpoint=opt.use_checkpoint, checkpoint_epoch=opt.checkpoint_epoch,
+                      val_epoch=opt.val_epoch, save_epoch=opt.save_epoch,
+                      local_rank=opt.local_rank, save_dir_path=opt.save_dir_path, lr=opt.lr,
+                      weight_decay=opt.weight_decay)
+
+    trainer.train()
+
+    if opt.local_rank == 0:
+        wandb.finish()
